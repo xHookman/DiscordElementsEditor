0ba098a6a5318d3df9ecdf4e1c4461bba3702ab1
Charly <jck0day@gmail.com>
2022-10-16 17:34:11 +0200

    Add files via upload

---
+++ ElementsEditors.plugin.js
@@ -0,0 +1,32 @@
+/**
+ * @name ElementsEditor
+ * @author Jck0day
+ * @description Allow you to edit elements just by clicking on a button !
+ * @version 0.0.1
+ */
+let btnDesignMode=null;
+let topBar=null;
+module.exports = class ElementsEditor {
+    start() {
+      // Called when the plugin is activated (including after reloads)
+	btnDesignMode = document.createElement("button");
+	btnDesignMode.textContent = "Edit";
+	btnDesignMode.addEventListener("click", () => {
+		if(document.designMode == 'on'){
+			document.designMode = 'off'
+		} else {
+			document.designMode = 'on'
+		}
+		BdApi.showToast("Editing mode : " + document.designMode);
+	});
+	document.querySelector(".toolbar-3_r2xA").append(btnDesignMode);
+	// This part re-adds it when removed
+	BdApi.onRemoved(btnDesignMode, () => {
+    		topBar.append(btnDesignMode);
+	});
+    }
+    stop() {
+      // Called when the plugin is deactivated
+	btnDesignMode.remove();
+    }
+}
